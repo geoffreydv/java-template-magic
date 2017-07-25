aacfe9e4e5b242e9339196ce9641d22ef3df66e2
Geoffrey De Vylder <geoffreydv@gmail.com>
2017-07-25 10:18:48 +0200

    Some organization code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,35 +4,33 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        SubjectKnowledgeBase knowledgeBase = buildKnowledgeBase();
+        generateTemplatesForClass(knowledgeBase, "OffloadedCalculationResult");
+    }
 
+    private static void generateTemplatesForClass(SubjectKnowledgeBase knowledgeBase, String className) {
+        ClassTemplateGenerator generator = new ClassTemplateGenerator();
+
+        List<String> subjectNames = new ArrayList<>();
+        subjectNames.add(className);
+
+        subjectNames.forEach(sn -> {
+            SourceClass result = knowledgeBase.findClass(sn);
+            if (result != null) {
+                String output = generator.generate(result, "C:\\generator\\templates", "migration_classes.vm");
+                System.out.println(output);
+            } else {
+                System.out.println("Class not found");
+            }
+        });
+    }
+
+    private static SubjectKnowledgeBase buildKnowledgeBase() throws IOException {
         SubjectKnowledgeBase knowledgeBase = new SubjectKnowledgeBase();
 
         List<String> sourceDirectories = new ArrayList<>();
         sourceDirectories.add("C:\\projects\\roots\\ifrs9\\core\\src\\main\\java\\be\\credo\\ifrs9\\core\\domain");
         knowledgeBase.build(sourceDirectories);
-
-        ClassTemplateGenerator generator = new ClassTemplateGenerator();
-
-        List<String> subjectNames = new ArrayList<>();
-        subjectNames.add("OffloadedRun");
-
-
-        subjectNames.forEach(sn -> {
-            SourceClass result = knowledgeBase.findClass(sn);
-            if (result != null) {
-                System.out.println("-------------------");
-                System.out.println("Result for " + sn);
-                System.out.println("-------------------");
-                String output = generator.generate(result, "C:\\generator\\templates", "migration_classes.vm");
-                System.out.println(output);
-                System.out.println();
-                System.out.println();
-                System.out.println();
-            } else {
-                System.out.println("Class not found");
-            }
-        });
-
-
+        return knowledgeBase;
     }
 }
